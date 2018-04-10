03933e37dcd9fdf823493b9a68b702b110df57b7
Eric Tu <tu.eric@hotmail.com>
2018-04-10 15:19:26 -0700

    add support for variable amounts of endpoints, read tests from file

--- index.js
+++ index.js
@@ -1,14 +1,68 @@
-const RPCSanityTest = require('./sanity.js')
-const test = new RPCSanityTest('https://mainnet.infura.io', 'http://10.0.20.231:8545');
+const RPCSanityTest = require('./sanity.js');
+const colors = require('colors');
+const fs = require('fs');
 
 
-function start(){
-    test.loadTest({method: 'eth_getLogs', params: [{from: 1700000, to: 1700005}]});
-    test.loadTest({method: 'eth_getBalance', params: ['0x07cff6218249a2351a174bdc1e5b1632e8e4e673', 'latest']});
-    test.loadTest({method: 'eth_getTransactionCount', params: ['0x07cff6218249a2351a174bdc1e5b1632e8e4e673', 'latest']});
-    test.loadTest({method: 'eth_estimateGas', params: [{to: '0x90C892a89daf937dD4c150120469c4EB146F164c', data: '0x60d586f8'}]});
-    test.loadTest({method: 'eth_blockNumber'});
+var argv = require('yargs')
+    .usage('Usage: $0 [options] endpoints1 ... endpointsn')
+    .demandCommand(2)
+    .boolean('v')
+    .alias('v', 'verbose')
+    .describe('v', 'Verbose Output')
+    .alias('d', 'directory')
+    .nargs('d', 1)
+    .demandOption(['d'])
+    .describe('d', 'Directory to load tests from')
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+function readdir (directory){
+    return new Promise ((resolve, reject) => {
+        fs.readdir(directory, (err, files) => {
+            if (err) reject(err);
+            resolve(files);
+        })
+    });
+}
+
+function readFile (filename) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, (err, data) =>{
+            if(err) reject(err);
+            resolve(data);
+        });
+    });
+}
+
+function buildSuite(directory) {
+    let suite = [];
+    let files = fs.readdirSync(directory)
+    files.forEach((file) => {
+        let data = fs.readFileSync(directory+'/'+file)
+        suite = suite.concat(JSON.parse(data.toString()).tests);
+    });
+    return suite;
+}
+
+function main() {
+    if (!argv.directory) {
+        console.error(argv)
+        console.error('You must enter a directory')
+        process.exit(0)
+    }
+    console.log(colors.yellow("Testing the following endpoints: "));
+    argv._.map((x) => {
+        console.log(colors.yellow(x))
+    });
+    const test = new RPCSanityTest(argv._, { verbose: argv.v });
+    const suite = buildSuite(argv.directory);
+    suite.forEach((testCase) => {
+        test.loadTest(testCase);
+    });
+
     test.executeTests();
 }
 
-start();
+
+main();
--- sanity.js
+++ sanity.js
@@ -5,18 +5,21 @@ const colors = require('colors');
 
 class RPCSanityTest{
     
-    constructor (urlOld, urlNew){
-        this.ethOld = new EthRPC(new HttpProvider(urlOld));
-        this.ethNew = new EthRPC(new HttpProvider(urlNew));
+    constructor (endpoints, options){
+        this.endpoints = endpoints;
+        this.options = options;
+        this.listOfProviders = endpoints.map((endpoint) =>{
+            return new EthRPC(new HttpProvider(endpoint));
+        })
         this.listOfTests = [];
     }
     
-    doubleQuery (options) {
-        return new Promise(async (resolve, reject) => {
-            let oldResult = await this.ethOld.sendAsync(options);
-            let newResult = await this.ethNew.sendAsync(options);
-            resolve({oldResult: oldResult, newResult: newResult});
-        })
+    //returns a list of promises
+    naryQuery (listOfProviders, options) {
+        let promises = listOfProviders.map((rpc) => {
+            return rpc.sendAsync(options);
+        });
+        return promises;
     }
     
     isEqual (a, b){
@@ -28,6 +31,7 @@ class RPCSanityTest{
     }
 
     async executeTests(){
+        console.log();
         console.log('STARTING TO EXECUTE TESTS')
         for (let i = 0 ; i < this.listOfTests.length ; i++){
             console.log(colors.rainbow('Starting Test ' + i + ": " + this.listOfTests[i].method));
@@ -36,12 +40,31 @@ class RPCSanityTest{
     }
 
     async testMethod (options) {
-        let result = await this.doubleQuery(options);
-        let match  = this.isEqual(result.oldResult, result.newResult);
+        let results = this.naryQuery(this.listOfProviders, options);
+        for (let i = 0 ; i < results.length ; i++) {
+            try{
+                results[i] = await results[i];
+            } catch (error){
+                console.log(colors.red('Error at endpoint: ' + this.endpoints[i] + ' with error message: '));
+                console.log(error);
+                console.log(colors.red('Test Case Failed'));
+                return;
+            }
+        }
+
+        let match  = this.isEqual(results[0], results[3]);
         if (match){
             console.log(colors.green('Test Case Pass'));
+            if (this.options.verbose) {
+                for ( let i = 0 ; i < results.length ; i ++){
+                    console.log(this.endpoints[i] + ": " + results[i]);
+                }
+            }
         } else {
             console.log(colors.red('Test Case Failed'));
+            for ( let i = 0 ; i < results.length ; i ++){
+                console.log(this.endpoints[i], ": ", results[i]);
+            }
         }
     }
 }
